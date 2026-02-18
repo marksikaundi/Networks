a12e5bab10299bdc248265c0135d0282a8b2c145
marksikaundi <emmanuelmark117@gmail.com>
2026-02-18 11:19:02 +0200

    rm

--- app/admin/posts/create/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import PostForm from "../[postId]/edit/page";
-
-export default function CreatePost() {
-  return <PostForm />;
-}
--- components/ui/clock.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import * as React from "react";
-import { Clock } from "lucide-react";
-
-export const ClockIcon = Clock;
